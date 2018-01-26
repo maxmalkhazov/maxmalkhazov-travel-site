8b659e5fcdabc1067e439be2fbaa8f2421ddf769
Max <maxmal86@gmail.com>
2018-01-25 19:36:41 -0600

    Completed reveal-on-scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
+import $ from "jquery";
 import MobileMenu from "./modules/mobileMenu.js";
+import RevealOnScroll from "./modules/RevealOnScroll.js";
 
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
